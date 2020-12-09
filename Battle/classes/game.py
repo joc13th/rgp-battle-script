44e520c13e1b306968bd0fbe71c2d6c839e76e0b
Jose <joc13th@gmail.com>
2020-12-09 18:28:44 -0300

    turning our magic into a class

--- Battle/classes/game.py
+++ Battle/classes/game.py
@@ -24,19 +24,19 @@ class Person:
         self.actions = ["Attack", "Magic", "Items"]
         self.name = name
 
-     def generate_damage(self):
+    def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
-    
-    def generate_spell_damage(self,i):
-         mgl = self.magic[i]["dmg"] - 5
-         mgh = self.magic[i]["dmg"] + 5
-         return random.randrange(mgl, mgh)
 
     def take_damage(self, dmg):
         self.hp -= dmg
         if self.hp < 0:
             self.hp = 0
         return self.hp
+
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
     
     def get_hp(self):
         return self.hp
@@ -50,14 +50,9 @@ class Person:
     def get_max_mp(self):
         return self.maxmp
     
-    def reduc_mp(self, cost):
+    def reduce_mp(self, cost):
         self.mp -= cost
 
-    def get_spell_name(self, i):
-        return self.magic[i]["name"]
-
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
 
     def choose_action(self):
         i = 1
@@ -68,7 +63,6 @@ class Person:
 
     def choose_magic(self):
         i = 1
-
         print("\n" + bcolors.OKBLUE + bcolors.BOLD + "    MAGIC:" + bcolors.ENDC)
         for spell in self.magic:
             print("        " + str(i) + ".", spell.name, "(cost:", str(spell.cost) + ")")
